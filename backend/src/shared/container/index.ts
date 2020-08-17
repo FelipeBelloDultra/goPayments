8fb2ed8ce33dae5bf3837c103152450e006f75a2
FelipeBelloDultra <felipe_bello_dultra@hotmail.com>
2020-08-17 20:25:44 -0300

    fix: Dependency injection cache provider

--- backend/src/shared/container/index.ts
+++ backend/src/shared/container/index.ts
@@ -1,6 +1,7 @@
 import { container } from 'tsyringe';
 
 import '@modules/users/providers';
+import './providers';
 
 import IPaymentsRepository from '@modules/payments/repositories/IPaymentsRepository';
 import PaymentsRepository from '@modules/payments/infra/typeorm/repositories/PaymentsRepository';
