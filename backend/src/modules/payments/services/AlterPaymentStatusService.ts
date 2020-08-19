5c346d2dd992ea7e647a8effd226c9e830cd91f0
FelipeBelloDultra <felipe_bello_dultra@hotmail.com>
2020-08-18 21:54:23 -0300

    feat: Cache implementation in list payments

--- backend/src/modules/payments/services/AlterPaymentStatusService.ts
+++ backend/src/modules/payments/services/AlterPaymentStatusService.ts
@@ -1,10 +1,10 @@
 import { injectable, inject } from 'tsyringe';
 
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import AppError from '@shared/errors/AppError';
+import IPaymentsRepository from '../repositories/IPaymentsRepository';
 
 import Payment from '../infra/typeorm/entities/Payment';
-
-import IPaymentsRepository from '../repositories/IPaymentsRepository';
 
 interface Request {
   id: string;
@@ -16,6 +16,9 @@ class AlterPaymentStatusService {
   constructor(
     @inject('PaymentsRepository')
     private paymentsRepository: IPaymentsRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({ id, user_id }: Request): Promise<Payment> {
@@ -30,6 +33,24 @@ class AlterPaymentStatusService {
     }
 
     await this.paymentsRepository.update(payment.id);
+
+    const recoveredPayments = await this.cacheProvider.recover<Payment[]>(
+      `payments-list:${user_id}`,
+    );
+
+    if (recoveredPayments) {
+      const newRecoveredPayments = recoveredPayments.map(newPayment => ({
+        ...newPayment,
+        status: newPayment.id === id ? 'paid' : newPayment.status,
+      }));
+
+      await this.cacheProvider.invalidatePrefix('payments-list');
+
+      await this.cacheProvider.save(
+        `payments-list:${user_id}`,
+        newRecoveredPayments,
+      );
+    }
 
     return payment;
   }
--- backend/src/modules/payments/services/ListAllPaymentService.ts
+++ backend/src/modules/payments/services/ListAllPaymentService.ts
@@ -1,8 +1,9 @@
 import { inject, injectable } from 'tsyringe';
 
-import Payment from '../infra/typeorm/entities/Payment';
-
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import IPaymentsRepository from '../repositories/IPaymentsRepository';
+
+import Payment from '../infra/typeorm/entities/Payment';
 
 interface Request {
   user_id: string;
@@ -14,11 +15,28 @@ class ListAppPaymentService {
   constructor(
     @inject('PaymentsRepository')
     private paymentsRepository: IPaymentsRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   // Cache provider implementation
   public async execute({ user_id, status }: Request): Promise<Payment[]> {
-    const payments = await this.paymentsRepository.findAll(user_id, status);
+    if (status) {
+      const payments = await this.paymentsRepository.findAll(user_id, status);
+
+      return payments;
+    }
+
+    let payments = await this.cacheProvider.recover<Payment[]>(
+      `payments-list:${user_id}`,
+    );
+
+    if (!payments) {
+      payments = await this.paymentsRepository.findAll(user_id, status);
+
+      await this.cacheProvider.save(`payments-list:${user_id}`, payments);
+    }
 
     return payments;
   }
